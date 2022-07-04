2c41c790048a9c8657dd60d6b27f00a2ebea300a
Cmarien <cmarien@debian-BULLSEYE-live-builder-AMD64>
2022-07-04 12:41:17 -0400

    RM

--- srcs/requirements/wordpress/srcs/install_wordpress.sh
+++
@@ -1,5 +0,0 @@
-adduser --disabled-password --gecos "" inception
-echo "wget https://wordpress.org/latest.tar.gz" | su - inception
-echo "tar zxf latest.tar.gz" | su - inception
-echo "rm latest.tar.gz" | su - inception
-echo "mv wordpress public_html" | su - inception
